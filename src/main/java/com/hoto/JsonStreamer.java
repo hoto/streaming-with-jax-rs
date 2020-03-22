6f5e8dd2c98be723c6879a819dc10fcb3e84cf31
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2020-03-22 14:51:37 +0000

    Add sleep but the streaming does not show up when curling

--- src/main/java/com/hoto/JsonStreamer.java
+++ src/main/java/com/hoto/JsonStreamer.java
@@ -9,12 +9,23 @@ public class JsonStreamer {
 
     public void stream(OutputStream stream) {
         JsonGenerator generator = Json.createGenerator(stream);
-        generator.writeStartObject();
+        generator.writeStartArray();
         for (int i = 0; i < 100; i++) {
+            generator.writeStartObject();
             generator.write("key", i);
+            generator.flush();
+            generator.writeEnd();
+            sleep();
         }
         generator.writeEnd();
-        generator.flush();
         generator.close();
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
